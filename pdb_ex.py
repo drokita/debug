aa910abb76ece9efb2b177e28a2c88cc21df4093
drokita <drokita@gmail.com>
2014-12-09 10:43:55 -0600

    post-debug
    Signed-off-by: drokita <drokita@gmail.com>

--- pdb_ex.py
+++ pdb_ex.py
@@ -1,18 +1,22 @@
 import sys
 from random import choice
+import pdb
 
 random1 = [1,2,3,4,5,6,7,8,9,10,11,12]
 random2 = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 while True:
    print "To exit the game type 'exit'"
-   answer = raw_input("What is {} times {}?".format(choice(random2), choice(random1))
+   num1 = choice(random2)
+   num2 = choice(random1)
+   pdb.set_trace()
+   answer = raw_input("What is {} times {}? ".format(num1, num2))
+#   pdb.set_trace()
 
    if answer == "exit":
       print "Now exiting the game!"
       sys.exit()
-
-   elif answer == choice(random2) * choice(random1):
+   elif int(answer) == num1 * num2:
       print "Correct!"
    else:
-      print "Wrong!
+      print "Wrong!"
