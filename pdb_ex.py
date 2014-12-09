7936b334e85d53e1761f82ee14c2735e0041d2f6
drokita <drokita@gmail.com>
2014-12-09 09:14:00 -0600

    Initialize
    Signed-off-by: drokita <drokita@gmail.com>

---
+++ pdb_ex.py
@@ -0,0 +1,18 @@
+import sys
+from random import choice
+
+random1 = [1,2,3,4,5,6,7,8,9,10,11,12]
+random2 = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+while True:
+   print "To exit the game type 'exit'"
+   answer = raw_input("What is {} times {}?".format(choice(random2), choice(random1))
+
+   if answer == "exit":
+      print "Now exiting the game!"
+      sys.exit()
+
+   elif answer == choice(random2) * choice(random1):
+      print "Correct!"
+   else:
+      print "Wrong!
